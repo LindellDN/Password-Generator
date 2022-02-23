784dbc1e6dd9523be600e972e5e5db80778bd4a5
Derek Lindell <dereklindell2@gmail.com>
2022-02-23 17:12:51 -0500

    js comment guide

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
+//declare varible for user input
+//prompt for password length (between 8-128 charecters)
+let pwLength = prompt ("please choose between a password length between 8 and 128 characters",'')
+//function for password crieteria input
+//min/max character if statements
+if (pwLength > 128) {
+  prompt ("generated passwords must be less than 128 characters",'' )
+}
+if (pwLength < 8) {
+  prompt ("generated passwords must be at least 8 characters", '')
+}
+//else statment to proceed to next prompt
+else {
+  let pwLength = prompt ("please choose between a password length between 8 and 128 characters",'')
+}
 
+//prompt for included character types
+//array of character types
+//minimum of 1 charecter type
+//prompt "you must select at least one chartater type"
+
+//display password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
